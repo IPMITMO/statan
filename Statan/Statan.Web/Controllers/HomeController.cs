2b8baea61bd493aa2b81e6960580ecc6519a590e
Artem Snezhny <getviruses@gmail.com>
2018-04-08 20:13:51 +0300

    Add page with results of specified project and version

--- Statan/Statan.Web/Controllers/HomeController.cs
+++ Statan/Statan.Web/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
 ﻿using Statan.Core.Repository;
+using System.Collections.Generic;
+using System.Data;
 using System.Web.Mvc;
+using System.Linq;
 
 namespace Statan.Web.Controllers
 {
@@ -18,10 +21,48 @@ namespace Statan.Web.Controllers
         }
 
         [HttpGet]
+        public ActionResult ProjectVersionResults()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
         public ActionResult Results()
         {
             var results = this.analyzerResultRepository.GetAll();
             return View(results);
+        }
+
+        [HttpPost]
+        public JsonResult NewChart()
+        {
+            var results = this.analyzerResultRepository.GetAll()
+                .Where(x => x.ProjectName == "thefuck");
+
+            //Creating data  
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Analyzer", System.Type.GetType("System.String"));
+            dt.Columns.Add("Messages", System.Type.GetType("System.Int32"));
+
+            foreach (var g in results.GroupBy(x => x.Origin))
+            {
+                DataRow dr = dt.NewRow();
+                dr["Analyzer"] = g.Key;
+                dr["Messages"] = g.Count();
+                dt.Rows.Add(dr);
+            }
+
+            //Looping and extracting each DataColumn to List<Object>  
+            List<object> iData = new List<object>();
+            foreach (DataColumn dc in dt.Columns)
+            {
+                List<object> x = new List<object>();
+                x = (from DataRow drr in dt.Rows select drr[dc.ColumnName]).ToList();
+                iData.Add(x);
+            }
+            //Source data returned as JSON  
+            return Json(iData, JsonRequestBehavior.AllowGet);
         }
     }
 }
